9a7363039d2de4911851a7df842064328801e89f
zhangjiancheng <zhangjiancheng10600@gmail.com>
2021-08-22 13:08:50 +0800

    添加cropper

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,33 @@
 import Base64 from "../modules/base64";
 import Home from "../modules/home";
 import JsonViewer from "../modules/json-viewer";
+import Cropper from "../modules/cropper";
 
 const routes = [
-  { path: "/", exact: true, name: "Home", component: Home, meta: { title: "Home" } },
-  { path: "/json-viewer", name: "JsonViewer", component: JsonViewer, meta: { title: "JsonViewer" } },
-  { path: "/base64", name: "Base64", component: Base64, meta: { title: "Base64" } },
+  {
+    path: "/",
+    exact: true,
+    name: "Home",
+    component: Home,
+    meta: { title: "Home" },
+  },
+  {
+    path: "/json-viewer",
+    name: "JsonViewer",
+    component: JsonViewer,
+    meta: { title: "JsonViewer" },
+  },
+  {
+    path: "/base64",
+    name: "Base64",
+    component: Base64,
+    meta: { title: "Base64" },
+  },
+  {
+    path: "/cropper",
+    name: "Cropper",
+    component: Cropper,
+    meta: { title: "Cropper" },
+  },
 ];
 export default routes;
